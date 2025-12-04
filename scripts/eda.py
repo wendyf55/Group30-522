cbd980636c91cc5a20f82c47df3b5fe956c0498c
Claudia <87799252+claudia-liauw@users.noreply.github.com>
2025-12-03 22:29:02 -0800

    save profile report

--- scripts/eda.py
+++ scripts/eda.py
@@ -1,14 +1,21 @@
 import click
 import os
 import pandas as pd
+from ydata_profiling import ProfileReport
 
 DEFAULT_TRAIN_PATH = '../data/processed/abalone_train.csv'
 DEFAULT_SAVE_DIR = '../results/eda'
 DEFAULT_TABLE_FILE = 'descriptive_stats.csv'
+DEFAULT_REPORT_FILE = 'pandas_profiling.html'
 
 
 def save_descriptive_table(train_df, descriptive_path):
     train_df.describe().round(2).to_csv(descriptive_path)
+
+
+def save_eda_report(train_df, report_path):
+    report = ProfileReport(train_df)
+    report.to_file(report_path)
 
 
 @click.command()
@@ -25,12 +32,17 @@ def save_descriptive_table(train_df, descriptive_path):
               type=str,
               default=DEFAULT_TABLE_FILE,
               help='Filename to save descriptive stats including extension')
-def main(train_path, save_dir, table_name):
+@click.option('--report-file',
+              type=str,
+              default=DEFAULT_REPORT_FILE,
+              help='Filename to save descriptive stats including extension')
+def main(train_path, save_dir, table_name, report_file):
     os.makedirs(save_dir, exist_ok=True)
 
     train_df = pd.read_csv(train_path)
 
     save_descriptive_table(train_df, os.path.join(save_dir, table_name))
+    save_eda_report(train_df, os.path.join(save_dir, report_file))
 
 
 if __name__ == "__main__":
